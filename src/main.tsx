0d6ae6811ad28b8c4efb482372aaf61ae2b51702
TAMV ONLINE MEXICO <120252809+OsoPanda1@users.noreply.github.com>
2026-02-13 00:55:58 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "@/contexts/AuthContext";
 import "./index.css";
 
 const container = document.getElementById("root");
-
 if (!container) {
   throw new Error("No se encontró el elemento #root en el DOM");
 }
@@ -16,9 +14,7 @@ const root = createRoot(container);
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
